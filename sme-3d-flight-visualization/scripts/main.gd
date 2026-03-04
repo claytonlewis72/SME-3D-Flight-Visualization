f512e63113db92e34f81ea08abbc3bab57eb70e9
Aramis Hernandez <ah1027406@wcupa.edu>
2026-03-03 21:35:38 -0500

    Merging Camera Branch to Rendering

--- sme-3d-flight-visualization/scripts/main.gd
+++ sme-3d-flight-visualization/scripts/main.gd
@@ -3,6 +3,11 @@
 extends Node
 
 func _ready():
+<<<<<<< Updated upstream
+=======
+	$CameraManager.set_target($"Rendering Manager/Drone") #Injection of drone for chased and fixed camera targeting
+	
+>>>>>>> Stashed changes
 	$IngestionManager.pose_received.connect(
 		$FlightPathRenderer.add_point
 	)
