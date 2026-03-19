31e6f85540f1804951eb2fb73962c4059157d90b
cl1007415 <cl1007415@wcupa.edu>
2026-03-19 16:07:39 -0400

    Final Commit w/ New Drone Model and Terrain

---
+++ sme-3d-flight-visualization/scripts/model_manager/VisualRoot.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@onready var plane_model = $plane2_9
+@onready var drone_model = $drone3
+
+var using_plane := true
+
+func _ready():
+	update_models()
+
+func _input(event):
+	if event.is_action_pressed("swap_vehicle"):
+		using_plane = !using_plane
+		update_models()
+
+func update_models():
+	plane_model.visible = using_plane
+	drone_model.visible = !using_plane
