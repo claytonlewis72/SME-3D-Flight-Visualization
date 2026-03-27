3b6e9531d7e59ae2435606c863163c00f1f3dbeb
Aramis Hernandez <ah1027406@wcupa.edu>
2026-03-26 21:26:49 -0400

    Bin Reading Writing

--- sme-3d-flight-visualization/binary_io.gd
+++ sme-3d-flight-visualization/binary_io.gd
@@ -1,20 +1,21 @@
 extends Node
 
 
-@export var TEST_PATH : String = "user://data/recorded_flightpath"
-var full_path = ProjectSettings.globalize_path(TEST_PATH)
+@export var TEST_PATH : String = "res://data/recorded_flightpath/test.bin"
+
 	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_write()
-#	_read()
+	_read()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _read() -> void:
+	var full_path = ProjectSettings.globalize_path(TEST_PATH)
 	var file := FileAccess.open(full_path, FileAccess.READ)
 	if file == null:
 		push_error("could not open file for writing.")
@@ -24,8 +25,8 @@ func _read() -> void:
 	print("Read back: ", value)
 
 func _write() -> void:
+	var full_path = ProjectSettings.globalize_path(TEST_PATH)
 	var file := FileAccess.open(full_path, FileAccess.WRITE)
-	
 	if file == null:
 		push_error("Could not open file for writing.")
 		return
