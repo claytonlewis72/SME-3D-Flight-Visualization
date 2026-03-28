75d68f5232e01ac14e6d4d7bbcde61814807ab7f
Aramis Hernandez <ah1027406@wcupa.edu>
2026-03-27 20:59:46 -0400

    Added Documentation to telemetry source

--- sme-3d-flight-visualization/scripts/ingestion/telemetry_source.gd
+++ sme-3d-flight-visualization/scripts/ingestion/telemetry_source.gd
@@ -1,18 +1,44 @@
-#Author: Aramis Hernandez
+#|------------------------------------------------------------------------------------
+#|   Unclassified
+#|------------------------------------------------------------------------------------
+#|
+#|   SME Solutions, Inc.
+#|   Copyright 2026 SME Solutions, Inc. All Rights Reserved
+#|   SME Solutions Proprietary Information
+#|
+#|------------------------------------------------------------------------------------
+#|
+#|   File Name   : telemetry_source.gd
+#|
+#|   Target      : Godot GDScript
+#|
+#|   Description :
+#|       Abstract base class for all telemetry sources.
+#|       Any source that feeds data into TelemetryManager must extend this class
+#|       and implement start() and stop().
+#|
+#|   POC         :
+#|       Aramis Hernandez
+#|
+#|------------------------------------------------------------------------------------
 
 @abstract
 class_name TelemetrySource
-
 extends Node
 
-
+## Emitted when a processed pose packet is ready to forward.
 signal telemetry_packet(data)
 
+## Begin producing telemetry data.
 @abstract func start()
+
+## Stop producing telemetry data.
 @abstract func stop()
 
+## Pause data emission without fully stopping the source.
 func pause():
 	pass
 
+## Resume data emission after a pause.
 func resume():
 	pass
