a8e4ddea73c19937f2ec112fae56c729a049d011
Aramis Hernandez <ah1027406@wcupa.edu>
2026-04-12 00:48:18 -0400

    Rotation Fix
    Fixed Rotation added from previous branches.

--- sme-3d-flight-visualization/scripts/rendering_manager.gd
+++ sme-3d-flight-visualization/scripts/rendering_manager.gd
@@ -2,14 +2,12 @@ extends Node3D
 
 @export var drone_path: NodePath = NodePath("Drone")
 
-# Keep pitch realistic
-@export var pitch_scale: float = 1.5
+# Visual scaling (for subtle telemetry)
+@export var pitch_scale: float = 100.0
+@export var roll_scale: float = 100.0
 
-# Roll can be exaggerated a bit for visibility
-@export var roll_scale: float = 10.0
-
-# Lower = smoother, higher = more responsive
-@export var rotation_smoothness: float = 2.0
+# Smoothing factor
+@export var rotation_smoothness: float = 3.0
 
 var drone: Node3D = null
 
@@ -23,10 +21,7 @@ func _ready() -> void:
 
 	print("Found Drone node:", drone.name)
 
-	if not TelemetryManager:
-		push_error("TelemetryManager not found")
-		return
-
+	#Connect to TelemetryManager (global singleton)
 	TelemetryManager.pose_received.connect(_on_pose_received)
 
 
@@ -41,16 +36,20 @@ func _on_pose_received(pos: Vector3, rot: Vector3, gap: bool, time: float) -> vo
 
 	# Copy incoming rotation
 	# Convert from Radians to Degrees
-	var target_X = rad_to_deg(rot.x)
-	# Rotation value is going the opposite direction so we reverse it's value
+	# Rotation value is going the opposite direction so we reverse it's values
+	# X = Roll
+	var target_X = rad_to_deg(rot.x*-1)
+	# Y = Pitch
 	var target_Y = rad_to_deg(rot.y*-1)
-	var target_Z = rad_to_deg(rot.z)
+	# Z = Yall
+	var target_Z = rad_to_deg(rot.z*-1)
+	
 	# Fix starting rotation value to line up with drawn line
-	var target_rot = Vector3(target_X, target_Y-1.58, target_Z)
+	var target_rot = Vector3(target_Y, target_Z-1.58, target_X)
 
 	## Apply rotation
 	drone.rotation = target_rot
- 
+	
 	# Optional debug
 	if gap:
 		print("Telemetry gap detected at t=", time)
